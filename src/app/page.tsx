af6b0cace2621e3bc820540268a259ffb5bb3f2f
Aman-web-dev <amanzhx1234+github@gmail.com>
2025-07-16 19:57:52 +0530

    Created Logic for random quiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,207 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+
+const DummyQuestions = [
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris",
+  },
+  {
+    question: "2 + 2 equals?",
+    options: ["3", "4", "5", "6"],
+    answer: "4",
+  },
+  {
+    question: "React is a ...?",
+    options: ["Library", "Framework", "Language", "IDE"],
+    answer: "Library",
+  },
+  {
+    question: "What is the capital of India?",
+    options: ["Mumbai", "Delhi", "Chennai", "Kolkata"],
+    answer: "Delhi",
+  },
+  {
+    question: "HTML stands for?",
+    options: [
+      "HyperText Markup Language",
+      "HyperText Makeup Language",
+      "Hyper Transfer Markup Language",
+      "HighText Markup Language",
+    ],
+    answer: "HyperText Markup Language",
+  },
+  {
+    question: "Which language runs in the browser?",
+    options: ["Python", "C++", "JavaScript", "Java"],
+    answer: "JavaScript",
+  },
+  {
+    question: "CSS is used for?",
+    options: [
+      "Data Storage",
+      "Styling Web Pages",
+      "Server Communication",
+      "Version Control",
+    ],
+    answer: "Styling Web Pages",
+  },
+  {
+    question: "Which is a JavaScript framework?",
+    options: ["Django", "Flask", "React", "Laravel"],
+    answer: "React",
+  },
+  {
+    question: "Which tag is used for headings in HTML?",
+    options: ["<head>", "<h1>", "<header>", "<title>"],
+    answer: "<h1>",
+  },
+  {
+    question: "Who developed React?",
+    options: ["Google", "Microsoft", "Facebook", "Amazon"],
+    answer: "Facebook",
+  },
+  {
+    question: "What does API stand for?",
+    options: [
+      "Application Programming Interface",
+      "Application Program Interaction",
+      "Applied Programming Interface",
+      "Application Programming Integration",
+    ],
+    answer: "Application Programming Interface",
+  },
+  {
+    question: "Which company owns GitHub?",
+    options: ["Google", "Microsoft", "Amazon", "Meta"],
+    answer: "Microsoft",
+  },
+  {
+    question: "What is the default port for HTTP?",
+    options: ["443", "21", "80", "25"],
+    answer: "80",
+  },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [questions, setQuestions] = useState(DummyQuestions);
+  const [currentQuestion, setCurrentQuestion] = useState();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  //could have maintain a single object to store all three score but for simplicity kept one
+  const [correct, setCorrect] = useState(0);
+  const [wrong, setWrong] = useState(0);
+  const [skipped, setSkipped] = useState(0);
+  const [selected, setSelected] = useState(null);
+
+  function getRandomQuestion() {
+    if (questions.length > 0) {
+      const toBeRemovedIndex = Math.floor(Math.random() * questions.length);
+      const selectedQuestion = questions[toBeRemovedIndex];
+      setCurrentQuestion(selectedQuestion);
+      setQuestions((prev) =>
+        prev.filter((_, index) => index !== toBeRemovedIndex)
+      );
+    }
+  }
+
+  function handleSubmit() {
+    if (selected === currentQuestion.answer) {
+      setCorrect((prev) => prev + 1);
+    } else {
+      setWrong((prev) => prev + 1);
+    }
+    getRandomQuestion();
+  }
+
+  function restartQuiz() {
+    setCorrect(0);
+    setWrong(0);
+    setSkipped(0);
+    setSelected(null); 
+    setQuestions(DummyQuestions);
+    setCurrentQuestion(null); 
+    
+//taking time in uodating so that old states gets updated and then this takes effect
+    setTimeout(() => {
+      const randomIndex = Math.floor(Math.random() * DummyQuestions.length);
+      const firstQuestion = DummyQuestions[randomIndex];
+      setCurrentQuestion(firstQuestion);
+      setQuestions(DummyQuestions.filter((_, index) => index !== randomIndex));
+    }, 0);
+  }
+
+const handleSkip=()=>{
+  if(questions.length>=0){
+setSkipped((prev)=>prev+=1);
+getRandomQuestion();
+  }
+}
+
+
+
+  useEffect(() => {
+    getRandomQuestion();
+  }, []);
+  return (
+    <div className="p-4 max-w-md mx-auto space-y-4">
+      <h1 className="text-xl font-bold">Quiz App</h1>
+      <p className="font-semibold">{currentQuestion?.question}</p>
+      {
+        currentQuestion &&      <div className="space-y-2">
+        {currentQuestion?.options.map((opt) => (
+          <div key={opt}>
+            <input
+              type="radio"
+              id={opt}
+              name="option"
+              value={opt}
+              checked={selected === opt}
+              onChange={(e) => setSelected(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <label htmlFor={opt} className="ml-2">
+              {opt}
+            </label>
+          </div>
+        ))}
+      </div>
+      }
+
+
+      <button
+      disabled={!(questions.length>=0)}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={handleSubmit}
+      >
+        Submit Answer
+      </button>
+      <button
+      disabled={!(questions.length>=0)}
+        className="bg-green-500 text-white px-4 py-2 rounded"
+        onClick={handleSkip}
+      >
+        Skip Question
+      </button>
+
+      {correct + wrong + skipped === DummyQuestions.length && (
+        <div>
+          <div className="space-y-1">
+            <h2 className="font-semibold">Scoreboard</h2>
+            <p>Correct Answers: {correct}</p>
+            <p>Wrong Answers: {wrong}</p>
+            <p>Skipped: {skipped}</p>
+          </div>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded"
+            onClick={restartQuiz}
           >
-            Read our docs
-          </a>
+            Restart Quiz
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+       )}
     </div>
   );
 }
